ccfcec9a078dd8a6c59c21f0b0a3a8a988c158d2
Milad Arbabi <arbabi.milad@gmail.com>
2025-05-20 15:46:13 +0200

    refactored the harmonyhomescreen and the playstoryscreen

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -24,6 +24,7 @@ declare module 'styled-components/native' {
       navBackground: string;
       iconActive: string;
       iconInactive: string;
+      error: string;
     };
     fonts: {
       regular: string;
