9d07e7fba1aae643e776d2219d8c4dcb96c4863f
Milad Arbabi <arbabi.milad@gmail.com>
2025-05-30 17:26:21 +0200

    tweaked the globe a little more

--- src/data/world-110m.ts
+++ src/data/world-110m.ts
@@ -1,13 +1,12 @@
 // src/data/world-110m.ts
 import { feature } from 'topojson-client';
 import type { FeatureCollection, Geometry, GeoJsonProperties } from 'geojson';
-// the correct path within world-atlas@2.x
-import land50 from 'world-atlas/land-50m.json';
+import land110 from 'world-atlas/land-110m.json';
 
  export const landFeatures: FeatureCollection<Geometry, GeoJsonProperties> = 
  feature( 
- land50 as any,
-(land50 as any).objects.land
+ land110 as any,
+(land110 as any).objects.land
  ) as any;
 
 export const sphereFeature = { type: 'Sphere' };
